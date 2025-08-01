92200ececdc71f62471393db30d0959a21a6322c
Sachin Chauhan <129787935+Chauhan713@users.noreply.github.com>
2025-08-01 18:35:26 +0530

    Delete test-me directory

--- test-me/server/src/routes/userRoutes.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { getUserData } from '../controllers/userController.js';
-
-const router = express.Router();
-router.get('/getUserData/:id', getUserData);
-
-export default router;
